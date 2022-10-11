f9e05badf483ffa9d37b8403b80f5f4963e02244
Elizaveta Yakovleva <lizaveta9223@gmail.com>
2022-10-11 20:51:11 +0300

    profile page upgraded

--- src/js/Sidebar.js
+++ src/js/Sidebar.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { useCookies } from 'react-cookie';
 
 import '.././css/Sidebar.css';
 
 function Sidebar(props){
+    const [cookies, removeCookie] = useCookies(["token"]);
+
+    const logout = () => {
+        removeCookie("token");
+        props.navigate('/login'); 
+    }
+
     return(
         <div className="sidenav">
-            <a href="#about">About</a>
-            <a href="#services">Services</a>
-            <a href="#clients">Clients</a>
-            <a href="#contact">Contact</a>
+            <a href="">
+                <i className="bi bi-person"></i>
+                <span>Profile</span>
+            </a>
+            <a href="">
+                <i className="bi bi-search"></i>
+                <span>Search...</span>
+            </a>
+            <a href="">
+            <i className="bi bi-check2-square"></i>
+                <span>Tickets</span>
+            </a>
+            <a href="">
+                <i className="bi bi-list"></i>    
+                <span>Projects</span>
+            </a> 
+            <hr/>
+            <a onClick={logout}>
+                <i className="bi bi-box-arrow-left"></i>
+                <span>Logout</span>  
+            </a>
         </div>               
     );
 }
