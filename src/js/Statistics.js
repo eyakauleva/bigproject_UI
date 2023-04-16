92cefcf78e3afc625389f908e6cbe9ffa0a45cff
eyakauleva <lizaveta9223@gmail.com>
2023-04-16 22:08:33 +0300

    statistics for logged time

--- src/js/Statistics.js
+++ src/js/Statistics.js
@@ -1,42 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import { useCookies } from 'react-cookie';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+import { format } from "date-fns";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 
 import {logout} from './Sidebar.js';
+import TimeChart from './TimeChart.js';
 import '.././css/Profile.css';
 
 export default function Statistics(props) {
     const[cookies, setCookie] = useCookies(["token", "employeeId"]);
+    const[errorMessage, setErrorMessage] = useState("");
     const[error, setError] = useState("");
 
     const labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    const options = {
-        legend: {
-            display: false
-        },
-        tooltips: {
-            callbacks: {
-              label: (item, data) => ('01.01.2023  '+ 8 + 'h')
-            }
-        },
-    }
-    const data = {
-        labels,
-        datasets: [
-          {
-            data: [8,8,8,8,8,0,0],
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'yellow', //transparent
-          },
-        ],
-    };
+    const[dates, setDates] = useState([]);
 
-    useEffect(() => {        
-        
+    useLayoutEffect(() => {
+        let datesBuf = [];
+        if(dates.length != 7) {
+            labels.forEach((label, idx) => {
+                let today = new Date();
+                let day = today.getDay() || 7;
+                if(day !== (idx + 1)) {
+                    today.setHours(-24 * (day - idx - 1));
+                }
+                datesBuf.push(format(today, 'yyyy-MM-dd').toString());
+            });
+            setDates(prevDates => [...prevDates, ...datesBuf]);
+        }
     }, []);
 
     const displayError = () => {
@@ -45,6 +40,20 @@ export default function Statistics(props) {
           alert(error);
           logout();
         }
+    }
+
+    const changeWeek = (previous) => {
+        let mult = 1;
+        if(previous){
+            mult = -1;
+        }
+        const newDates = [...dates];
+        newDates.forEach((date, idx) => {
+            let buf = new Date(date);
+            buf.setHours(mult * 24 * 7);
+            newDates[idx] = format(buf, 'yyyy-MM-dd').toString();
+        });
+        setDates(newDates);
     }
 
     return (
@@ -56,15 +65,15 @@ export default function Statistics(props) {
                     <Tab eventKey="my" title="My">
                         <div>Week activity</div>
                         <div style={{display: 'flex', flexDirection: 'row'}}>
-                            <div>
-                                <i class="bi bi-caret-left"></i>
+                            <div onClick={()=>changeWeek(true)}>
+                                {dates.length == 7 ? format(new Date(dates[0]), 'dd.MM.yyyy') : ''}<i class="bi bi-caret-left"></i>
                             </div>
-                            <div>
-                                <i class="bi bi-caret-right"></i>
+                            <div onClick={()=>changeWeek(false)}>
+                                <i class="bi bi-caret-right"></i>{dates.length == 7 ? format(new Date(dates[6]), 'dd.MM.yyyy') : ''}
                             </div>
                         </div>
                         <div style={{height: '400px', width: '600px'}}>
-                            <Line options={options} data={data} />
+                            <TimeChart dates={dates} labels={labels} />
                         </div>
                     </Tab>
                     <Tab eventKey="?Project?" title="?Project?">
